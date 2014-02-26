4c4117c41011822a3abe9e844dd07e6bf1cb0d2d
Tarjei Husøy <tarjei@roms.no>
2014-02-26 18:41:26 +0100

    Add setup tutorial
    On first use, will prompt for database to use, and save config to user
    home directory, where the database will be stored in the case of a local
    setup.

--- pwm.py
+++ pwm.py
@@ -2,12 +2,20 @@ import argparse
 import getpass
 import hashlib
 import os
+import textwrap
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+try:
+    import configparser
+except:
+    # python 2
+    import ConfigParser as configparser
+    input = raw_input
+
+
 Base = declarative_base()
-db = sa.create_engine('sqlite:///testdb.sqlite')
 
 
 class DomainPassword(Base):
@@ -35,7 +43,6 @@ class DomainPassword(Base):
         return 'DomainPassword(domain=%s, salt=%s)' % (self.domain, self.salt)
 
 
-Base.metadata.create_all(db)
 
 def main():
     args = get_args()
@@ -49,16 +56,52 @@ def get_args():
     argparser.add_argument('domain',
         help='The domain to retrieve the password for',
     )
-    argparser.add_argument('-d', '--database', metavar='<database>',
-        help='The database to store the salts in',
-    )
-    return argparser.parse_args()
+    rc_dir = os.path.join(os.path.expanduser('~'), '.pwm')
+    args = argparser.parse_args()
+    if not os.path.exists(rc_dir):
+        os.makedirs(rc_dir)
+        run_setup()
+    config = read_config()
+    args.database = config.get('pwm', 'database')
+    return args
+
+
+def run_setup():
+    print textwrap.dedent("""\
+        Hi, it looks like it's the first time you're using pwm on this machine. Let's take a little
+        moment to set things up before we begin.""")
+    db_uri = input('Which database do you want to use? Default: local') or 'local'
+    rc_dir = os.path.join(os.path.expanduser('~'), '.pwm')
+
+    if db_uri == 'local':
+
+        # normalize windows-style paths for sqlalchemy:
+        rc_dir = rc_dir.replace('\\', '/')
+
+        # Create the local database
+        db_uri = 'sqlite:///%s/db.sqlite' % rc_dir
+        db = sa.create_engine(db_uri)
+        Base.metadata.create_all(db)
+
+    config = configparser.RawConfigParser()
+    config.add_section('pwm')
+    config.set('pwm', 'database', db_uri)
+
+    with open(os.path.join(rc_dir, 'config'), 'w') as configfile:
+        config.write(configfile)
+
+
+def read_config():
+    config_file = os.path.join(os.path.expanduser('~'), '.pwm', 'config')
+    config = configparser.RawConfigParser()
+    config.read(config_file)
+    return config
 
 
 def get_domain_password(args):
     session = get_db_session(args.database)
-    dp = get_salt(session, args.domain)
-    return dp
+    domain_password = get_salt(session, args.domain)
+    return domain_password
 
 
 def get_salt(session, domain):
@@ -71,6 +114,7 @@ def get_salt(session, domain):
 
 
 def get_db_session(database):
+    db = sa.create_engine(database)
     DBSession = sessionmaker(bind=db)
     session = DBSession()
     return session
