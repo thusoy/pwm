25936b89f1dd1bef9cbf9643606a442b0cd2f091
Tarjei Husøy <tarjei@roms.no>
2014-02-27 01:16:18 +0100

    Add ability to use REST API backends

--- pwm.py
+++ pwm.py
@@ -1,11 +1,15 @@
 import argparse
 import getpass
 import hashlib
+import httplib
+import logging.config
 import os
+import requests
 import textwrap
 import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from logging import getLogger
 
 try:
     import configparser
@@ -16,6 +20,7 @@ except:
 
 
 Base = declarative_base()
+_logger = getLogger('pwm')
 
 
 class DomainPassword(Base):
@@ -56,13 +61,20 @@ def get_args():
     argparser.add_argument('domain',
         help='The domain to retrieve the password for',
     )
+    argparser.add_argument('-v', '--verbose', action='store_true',
+        help='Increase verbosity',
+    )
     rc_dir = os.path.join(os.path.expanduser('~'), '.pwm')
     args = argparser.parse_args()
+    _init_logging(verbose=args.verbose)
     if not os.path.exists(rc_dir):
         os.makedirs(rc_dir)
         run_setup()
     config = read_config()
     args.database = config.get('pwm', 'database')
+    client_certificate = os.path.join(rc_dir, config.get('pwm', 'certificate'))
+    client_key = os.path.join(rc_dir, config.get('pwm', 'key'))
+    args.auth = (client_certificate, client_key)
     return args
 
 
@@ -99,8 +111,23 @@ def read_config():
 
 
 def get_domain_password(args):
-    session = get_db_session(args.database)
-    domain_password = get_salt(session, args.domain)
+    protocol = args.database.split(':', 1)[0]
+    if protocol in ('https', 'http'):
+        return get_salt_from_rest_api(args.database, args.domain, auth=args.auth)
+    else:
+        return get_salt_from_db(args.database, args.domain)
+
+
+def get_salt_from_rest_api(api_url, domain, auth):
+    payload = {'domain': domain}
+    response = requests.get(api_url + '/get', params=payload)
+    domain_password = DomainPassword(domain=domain, salt=response.json()['salt'])
+    return domain_password
+
+
+def get_salt_from_db(database, domain):
+    session = get_db_session(database)
+    domain_password = get_salt(session, domain)
     return domain_password
 
 
@@ -118,3 +145,37 @@ def get_db_session(database):
     DBSession = sessionmaker(bind=db)
     session = DBSession()
     return session
+
+
+def _init_logging(verbose=False):
+    """ Initialize loggers. """
+    config = {
+        'version': 1,
+        'formatters': {
+            'normal': {
+                'format': '%(asctime)s %(levelname)-10s %(name)s %(message)s',
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+                'level': 'DEBUG',
+                'formatter': 'normal',
+                'stream': 'ext://sys.stdout',
+            }
+        },
+        'loggers': {
+            'pwm': {
+                'level': 'DEBUG' if verbose else 'INFO',
+                'handlers': ['console'],
+                'propagate': True,
+            },
+            'requests.packages.urllib3': {
+                'level': 'INFO' if verbose else 'WARNING',
+                'handlers': ['console'],
+                'propagate': True,
+            }
+        }
+    }
+    logging.config.dictConfig(config)
+    httplib.HTTPConnection.debuglevel = 1 if verbose else 0
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ from setuptools import setup, find_packages
 
 install_requires = [
     'sqlalchemy',
+    'requests',
 ]
 
 if sys.version_info < (2, 7, 0):
