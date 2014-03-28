71c200b1a1440821a74ed80cc9c173ad139d54bc
Tarjei Husøy <tarjei@roms.no>
2014-03-28 22:35:35 +0100

    Bump to 0.1.5

--- pwm/__init__.py
+++ pwm/__init__.py
@@ -7,7 +7,7 @@
 """
 # pylint: disable=unused-import
 
-__version__ = '0.1.4' # When bumping, also bump version in setup.py
+__version__ = '0.1.5' # When bumping, also bump version in setup.py
 
 from .core import (
     Domain,
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ extras['dev'] = extras['test'] + ['tox', 'nosy', 'sphinx']
 
 setup(
     name='pwm',
-    version='0.1.4', # When bumping, also bump pwm.__version__
+    version='0.1.5', # When bumping, also bump pwm.__version__
     author='Tarjei Husøy',
     author_email='tarjei@roms.no',
     url='https://github.com/thusoy/pwm',
